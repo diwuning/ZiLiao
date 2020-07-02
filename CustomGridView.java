5d05033b290be7772f0918160f8caabbfb1147b8
diwuning <153964989@qq.com>
2020-07-02 15:08:34 +0800

    Create CustomGridView.java

---
+++ CustomGridView.java
@@ -0,0 +1,23 @@
+package com.nmpa.nmpaapp.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+public class CustomGridView extends GridView {
+    public CustomGridView(Context context) {
+        super(context);
+    }
+    
+    public CustomGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // TODO Auto-generated method stub
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
