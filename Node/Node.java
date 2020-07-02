bf2ce3ee45345fa9a88982c7dbd655e1d16e4077
diwuning <153964989@qq.com>
2020-07-02 18:06:16 +0800

    树状图的节点

--- Node/Node.java
+++ Node/Node.java
@@ -6,4 +6,212 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Node<T, B> implements Serializable {
+  /**
+     * 传入的实体对象
+     */
+    public B bean;
+    /**
+     * 设置开启的图片
+     */
+    public int iconExpand = -1;
+    /**
+     * 设置关闭的图片
+     */
+    public int iconNoExpand = -1;
+  
+    private T id;
+    /**
+     * 根节点pId为0
+     */
+    private T pId;
+
+    private String name;
+
+    /**
+     * 当前的级别
+     */
+    private int level;
+
+    /**
+     * 是否展开
+     */
+    private boolean isExpand = false;
+
+    private int icon = -1;
+
+    /**
+     * 下一级的子Node
+     */
+    private List<Node> children = new ArrayList<>();
+
+    /**
+     * 父Node
+     */
+    private Node parent;
+  /**
+     * 是否被checked选中
+     */
+    private boolean isChecked;
+    /**
+     * 是否为新添加的
+     */
+    public boolean isNewAdd = true;
+    /**
+     * 单选标记
+     */
+    public boolean isSingle;
+  
+  /**
+     * 支部code
+     */
+    public String braCode;
+
+    public Node(T id, T pId, String name) {
+        super();
+        this.id = id;
+        this.pId = pId;
+        this.name = name;
+    }
+
+    public Node(T id, T pId, String name, B bean) {
+        super();
+        this.id = id;
+        this.pId = pId;
+        this.name = name;
+        this.bean = bean;
+    }
+
+    public String getBraCode() {
+        return braCode;
+    }
+
+    public void setBraCode(String braCode) {
+        this.braCode = braCode;
+    }
+
+    public boolean isSingle() {
+        return isSingle;
+    }
+  
+    public void setSingle(boolean single) {
+        isSingle = single;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean isChecked) {
+        this.isChecked = isChecked;
+    }
+
+    public Node() {
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+  
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+
+    public T getpId() {
+        return pId;
+    }
+
+    public void setpId(T pId) {
+        this.pId = pId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+  
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isExpand() {
+        return isExpand;
+    }
+
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Node> children) {
+        this.children = children;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+  
+    /**
+     * 是否为跟节点
+     *
+     * @return
+     */
+    public boolean isRoot() {
+        return parent == null;
+    }
+
+    /**
+     * 判断父节点是否展开
+     *
+     * @return
+     */
+    public boolean isParentExpand() {
+        if (parent == null){
+            return false;
+        }
+        return parent.isExpand();
+    }
+
+    /**
+     * 是否是叶子界点
+     *
+     * @return
+     */
+    public boolean isLeaf() {
+        return children.size() == 0;
+    }
+
+    /**
+     * 获取level
+     */
+    public int getLevel() {
+        return parent == null ? 0 : parent.getLevel() + 1;
+    }
+  
+    /**
+     * 设置展开
+     *
+     * @param isExpand
+     */
+    public void setExpand(boolean isExpand) {
+        this.isExpand = isExpand;
+        if (!isExpand) {
+            for (Node node : children) {
+                node.setExpand(false);
+            }
+        }
+    }
 }
