f900a97265326cd0899f9f0393a0b6a8587a24cc
diwuning <153964989@qq.com>
2020-07-08 11:13:02 +0800

    Create RecordingService.java

---
+++ Huanxin/voice/record/RecordingService.java
@@ -0,0 +1,22 @@
+package com.nmpa.nmpaapp.modules.huanxin.record;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import com.hyphenate.EMError;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.util.EMLog;
+import com.hyphenate.util.PathUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class RecordingService extends Service {
+}
