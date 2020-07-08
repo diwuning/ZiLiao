1e9a49a739f72d62791d0a2a8b8228da24f3e367
diwuning <153964989@qq.com>
2020-07-08 11:10:25 +0800

    Create RecordUtil.java

---
+++ Huanxin/voice/record/RecordUtil.java
@@ -0,0 +1,5 @@
+public class RecordUtil {
+    private static final int STATE_NO_PERMISSION = 1;
+    private static final int STATE_RECORDING = 2;
+    private static final int STATE_SUCCESS = 3;
+}
