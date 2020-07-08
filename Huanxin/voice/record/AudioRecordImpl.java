b04227c516a00600de1c6cd0fa0e05b1d94612ac
diwuning <153964989@qq.com>
2020-07-08 11:25:20 +0800

    Create AudioRecordImpl.java

---
+++ Huanxin/voice/record/AudioRecordImpl.java
@@ -0,0 +1,11 @@
+package com.nmpa.nmpaapp.modules.huanxin.record;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import com.nmpa.nmpaapp.utils.FileLoadUtils;
+
+public class AudioRecordImpl {
+}
