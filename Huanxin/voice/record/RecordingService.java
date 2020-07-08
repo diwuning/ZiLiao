f5d4bbe5a6a2e0738f1355724f1458f270de3a45
diwuning <153964989@qq.com>
2020-07-08 11:16:58 +0800

    Update RecordingService.java

--- Huanxin/voice/record/RecordingService.java
+++ Huanxin/voice/record/RecordingService.java
@@ -19,4 +19,151 @@ import java.io.IOException;
 import java.util.Date;
 
 public class RecordingService extends Service {
+    private static final String TAG = "RecordingService";
+
+    private String mFileName;
+    private String mFilePath;
+
+    private MediaRecorder mRecorder;
+
+    private long mStartingTimeMillis;
+    private long mElapsedMillis;
+  
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(TAG, "onStartCommand");
+        startRecording();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mRecorder != null) {
+            stopRecording();
+        }
+        super.onDestroy();
+    }
+  
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.e(TAG, "onBind");
+        return null;
+    }
+
+    // 开始录音
+    public void startRecording() {
+        Log.e(TAG, "startRecording");
+        setFileNameAndPath();
+
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4); //录音文件保存的格式，这里保存为 mp4
+        mRecorder.setOutputFile(mFilePath); // 设置录音文件的保存路径
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mRecorder.setAudioChannels(1);
+        // 设置录音文件的清晰度
+        mRecorder.setAudioSamplingRate(44100);
+        mRecorder.setAudioEncodingBitRate(192000);
+
+        try {
+            mRecorder.prepare();
+            mRecorder.start();
+            mStartingTimeMillis = System.currentTimeMillis();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+    }
+
+    MediaRecorder recorder;
+    private File file;
+    private String voiceFilePath;
+    private boolean isRecording = false;
+    private long startTime;
+    public String startRecording(Context appContext) {
+        file = null;
+        try {
+            // need to create recorder every time, otherwise, will got exception
+            // from setOutputFile when try to reuse
+            if (recorder != null) {
+                recorder.release();
+                recorder = null;
+            }
+            recorder = new MediaRecorder();
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.setAudioChannels(1); // MONO
+            recorder.setAudioSamplingRate(8000); // 8000Hz
+            recorder.setAudioEncodingBitRate(64); // seems if change this to
+          // 128, still got same file
+            // size.
+            // one easy way is to use temp file
+            // file = File.createTempFile(PREFIX + userId, EXTENSION,
+            // User.getVoicePath());
+            String voiceFileName = EMClient.getInstance().getCurrentUser()+System.currentTimeMillis()+".amr";
+            voiceFilePath = PathUtil.getInstance().getVoicePath() + "/" + voiceFileName;
+            file = new File(voiceFilePath);
+          recorder.setOutputFile(file.getAbsolutePath());
+            recorder.prepare();
+            isRecording = true;
+            recorder.start();
+        } catch (IOException e) {
+            EMLog.e("voice", "prepare() failed");
+        }
+      
+        startTime = new Date().getTime();
+        EMLog.d("voice", "start voice recording to file:" + file.getAbsolutePath());
+        return file == null ? null : file.getAbsolutePath();
+    }
+
+    // 设置录音文件的名字和保存路径
+    public void setFileNameAndPath() {
+        int count = 0;
+        File f;
+        do {
+            count++;
+//            mFileName = getString(R.string.app_name) + "_" + (System.currentTimeMillis()) + ".mp4";
+            mFileName = (System.currentTimeMillis()) + ".mp4";
+            mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFilePath += "/SoundRecorder/" + mFileName;
+            f = new File(mFilePath);
+        } while (f.exists() && !f.isDirectory());
+    }
+  
+    public int stopRecoding() {
+        if(recorder != null){
+            isRecording = false;
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+
+            if(file == null || !file.exists() || !file.isFile()){
+                return EMError.FILE_INVALID;
+            }
+            if (file.length() == 0) {
+                file.delete();
+                return EMError.FILE_INVALID;
+            }
+            int seconds = (int) (new Date().getTime() - startTime) / 1000;
+            EMLog.d("voice", "voice recording finished. seconds:" + seconds + " file length:" + file.length());
+            return seconds;
+        }
+        return 0;
+    }
+  
+    // 停止录音
+    public void stopRecording() {
+        Log.e(TAG, "stopRecording");
+        mRecorder.stop();
+        mElapsedMillis = (System.currentTimeMillis() - mStartingTimeMillis);
+        mRecorder.release();
+        getSharedPreferences("sp_name_audio", MODE_PRIVATE)
+                .edit()
+                .putString("audio_path", mFilePath)
+                .putLong("elpased", mElapsedMillis)
+                .apply();
+        mRecorder = null;
+    }
+
 }
