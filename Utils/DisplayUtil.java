627d2d131336150a6e45946b70d382b34ee703b5
diwuning <153964989@qq.com>
2020-07-09 14:43:48 +0800

    Create DisplayUtil.java

---
+++ Utils/DisplayUtil.java
@@ -0,0 +1,22 @@
+package com.nmpa.nmpaapp.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * Created by aaron on 16/8/3.
+ */
+ public class DisplayUtil
+{
+
+    public static int screenWidthPx; //屏幕宽 px
+    public static int screenhightPx; //屏幕高 px
+    public static float density;//屏幕密度
+    public static int densityDPI;//屏幕密度
+    public static float screenWidthDip;//  dp单位
+    public static float screenHightDip;//  dp单位
+}
