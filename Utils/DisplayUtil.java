d4a48d98b97c0e52d7f76c11cd517fd2e828a513
diwuning <153964989@qq.com>
2020-07-09 14:49:49 +0800

    Update DisplayUtil.java

--- Utils/DisplayUtil.java
+++ Utils/DisplayUtil.java
@@ -36,4 +36,162 @@ import android.view.WindowManager;
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
+  
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels;
+    }
+  
+    public static int getScreenWidth(Context context) {
+//        WindowManager wm= (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+//        return wm.getDefaultDisplay().getWidth();
+        return context.getApplicationContext().getResources().getDisplayMetrics().widthPixels;
+    }
+  
+  
+    public static void hideNavKey(Activity activity) {
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            View v = activity.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = activity.getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+    public static void showNavKey(Activity activity, int systemUiVisibility) {
+        activity.getWindow().getDecorView().setSystemUiVisibility(systemUiVisibility);
+    }
+  
+    /**
+     * 根据手机的分辨率从 sp 的单位 转成为 px
+     *
+     * @param context 上下文
+     * @param spValue sp value
+     * @return px value
+     */
+    public static int sp2Px(Context context, float spValue) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spValue, metrics);
+    }
+  
+  
+    /**
+     * 获取虚拟按键栏高度
+     *
+     * @param context 上下文
+     * @return 虚拟按键高度
+     */
+    public static int getNavigationBarHeight(Context context) {
+        int result = 0;
+        if (checkDeviceHasNavigationBar(context)) {
+            Resources res = context.getResources();
+            int resourceId = res.getIdentifier("navigation_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = res.getDimensionPixelSize(resourceId);
+            }
+        }
+        return result;
+    }
+  
+    /**
+     * 获取是否存在NavigationBar(虚拟按键)
+     *
+     * @param context 上下文
+     * @return true-存在  false-不存在
+     */
+    public static boolean checkDeviceHasNavigationBar(Context context) {
+        boolean hasNavigationBar = false;
+        Resources rs = context.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (id > 0) {
+            hasNavigationBar = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                hasNavigationBar = false;
+            } else if ("0".equals(navBarOverride)) {
+                hasNavigationBar = true;
+            }
+        } catch (Exception e) {
+
+        }
+        return hasNavigationBar;
+
+    }
+  
+    /**
+     * 获取手机状态栏高度
+     *
+     * @param context 上下文对象
+     * @return 手机状态栏高度
+     */
+    public static int getStatusBarHeight(Context context) {
+        Class<?> c = null;
+        Object obj = null;
+        Field field = null;
+        int x = 0, statusBarHeight = 0;
+        try {
+            c = Class.forName("com.android.internal.R$dimen");
+            obj = c.newInstance();
+            field = c.getField("status_bar_height");
+            x = Integer.parseInt(field.get(obj).toString());
+            statusBarHeight = context.getResources().getDimensionPixelSize(x);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return statusBarHeight;
+    }
+  
+    /**
+     * 获取设备的制造商
+     *
+     * @return 设备制造商
+     */
+    public static String getDeviceManufacture() {
+        return android.os.Build.MANUFACTURER;
+    }
+  
+    /**
+     * 获取设备名称
+     *
+     * @return 设备名称
+     */
+    public static String getDeviceName() {
+        return android.os.Build.MODEL;
+    }
+
+    /**
+     * 获取系统版本号
+     *
+     * @return 系统版本号
+     */
+    public static String getSystemVersion() {
+        return android.os.Build.VERSION.RELEASE;
+    }
+  
+    /**
+     * 获取设备号
+     *
+     * @return 设备唯一编码  获取不到时返回""
+     */
+    public static String getDeviceId() {
+        String deviceId;
+        try {
+            TelephonyManager telephonyManager = (TelephonyManager) LibApp.getContext().getSystemService(Context
+                    .TELEPHONY_SERVICE);
+            deviceId = telephonyManager.getDeviceId();
+        } catch (SecurityException e) {
+            deviceId = Settings.Secure.getString(LibApp.getContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+        }
+        return TextUtils.isEmpty(deviceId) ? "" : deviceId;
+    }
 }
