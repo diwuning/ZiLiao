091e0be214ad857bcf94815809317c171dd9eded
diwuning <153964989@qq.com>
2020-07-09 14:44:39 +0800

    Update DisplayUtil.java

--- Utils/DisplayUtil.java
+++ Utils/DisplayUtil.java
@@ -19,4 +19,21 @@ import android.view.WindowManager;
     public static int densityDPI;//屏幕密度
     public static float screenWidthDip;//  dp单位
     public static float screenHightDip;//  dp单位
+   
+  
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+  
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 }
