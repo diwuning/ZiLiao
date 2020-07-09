cb757b51e03f911872466a097430b37342837b61
diwuning <153964989@qq.com>
2020-07-09 14:50:46 +0800

    Create SPUtils.java

---
+++ Utils/SPUtils.java
@@ -0,0 +1,16 @@
+package com.nmpa.nmpaapp.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * Created by Amosnail on 2016/5/21.
+ * Sharedpreferenced的工具类
+ */
+public class SPUtils {
+}
