9fef70190a837371fc3fa9ed546ee25acfa807b8
diwuning <153964989@qq.com>
2020-07-09 16:22:19 +0800

    Create TreeHelper.java

---
+++ Node/TreeHelper.java
@@ -0,0 +1,16 @@
+package com.nmpa.nmpaapp.modules.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeHelper {
+    /**
+     * 传入node  返回排序后的Node
+     *
+     * @param datas              传入数据
+     * @param defaultExpandLevel 默认展开等级
+     * @return
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     */
+ }
