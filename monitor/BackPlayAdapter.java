6d5c416a4f5089bd8e7b9c7f495f59fbbad6a3cd
diwuning <153964989@qq.com>
2020-07-09 11:29:07 +0800

    回看的视频列表适配器

--- monitor/BackPlayAdapter.java
+++ monitor/BackPlayAdapter.java
@@ -18,4 +18,61 @@ public class BackPlayAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     private static final String TAG = "CheckPlanAdapter";
     private Context mContext;
     private List<PlayBean> playBeans;
+    
+    public BackPlayAdapter(Context mContext, List<PlayBean> planBeans) {
+        this.mContext = mContext;
+        this.playBeans = planBeans;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.back_play_item,parent,false);
+        PlanHolder planHolder = new PlanHolder(view);
+        return planHolder;
+    }
+    
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        PlayBean planBean = playBeans.get(position);
+        PlanHolder planHolder = (PlanHolder) holder;
+//        planHolder.tv_bDate.setText(planBean.getRecordDate());
+        planHolder.tv_bName.setText(planBean.getName());
+
+        Log.d(TAG,"planBean.getName()="+planBean.getName());
+        planHolder.ll_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ARouter.getInstance().build(Page.ACTIVITY_BACK_PLAYER).withString("path",planBean.getPath()).navigation();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return playBeans == null? 0: playBeans.size();
+    }
+
+    class PlanHolder extends RecyclerView.ViewHolder {
+        private TextView tv_bDate,tv_bName;
+        private LinearLayout ll_play;
+
+        public PlanHolder(@NonNull View itemView) {
+            super(itemView);
+//            tv_bDate = itemView.findViewById(R.id.tv_bDate);
+            tv_bName = itemView.findViewById(R.id.tv_bName);
+            ll_play = itemView.findViewById(R.id.ll_play);
+        }
+    }
+
+    public OnClickItemListener onClickItemListener;
+    public void setOnItemClickListener(OnClickItemListener onClickBottomListener) {
+        this.onClickItemListener = onClickBottomListener;
+    }
+    public interface OnClickItemListener{
+        /**
+         * 点击确定按钮事件
+         */
+        public void onItemClick(int position);
+    }
 }
