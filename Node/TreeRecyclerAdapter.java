e98343ff83a7823fee45f89d4a7a56cce6972cde
diwuning <153964989@qq.com>
2020-07-09 16:12:21 +0800

    Create TreeRecyclerAdapter.java

---
+++ Node/TreeRecyclerAdapter.java
@@ -0,0 +1,15 @@
+package com.nmpa.nmpaapp.modules.tree;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+import com.nmpa.nmpaapp.utils.DisplayUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class TreeRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+}
