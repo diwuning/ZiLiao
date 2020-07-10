206a39ce0e56c95640df008e8bf5f3264eee59a8
diwuning <153964989@qq.com>
2020-07-10 09:30:42 +0800

    Update AssetsUtils.java

--- Utils/AssetsUtils.java
+++ Utils/AssetsUtils.java
@@ -11,4 +11,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class AssetsUtils {
+    public static Object assetsFile2Bean(String assetsName, Class beanClass) {
+        //将json数据变成字符串
+        StringBuilder result = new StringBuilder();
+        try {
+            //获取assets资源管理器
+            AssetManager assetManager = MyApplication.getInstance().getAssets();
+            //通过管理器打开文件并读取
+            BufferedReader bf = new BufferedReader(new InputStreamReader(
+                    assetManager.open(assetsName)));
+            String line;
+            while ((line = bf.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        if (!result.toString().equals("")) {
+            return new Gson().fromJson(result.toString(), beanClass);
+        } else {
+            return null;
+        }
+    }
+  
+    public static Object assetsFile2Data(String assetsName) {
+        //将json数据变成字符串
+        StringBuilder result = new StringBuilder();
+        try {
+            //获取assets资源管理器
+            AssetManager assetManager = MyApplication.getInstance().getAssets();
+            //通过管理器打开文件并读取
+            BufferedReader bf = new BufferedReader(new InputStreamReader(
+                    assetManager.open(assetsName)));
+            String line;
+            while ((line = bf.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        if (!result.toString().equals("")) {
+            return new Gson().fromJson(result.toString(), InfoAlertBean.class);
+        } else {
+            return null;
+        }
+    }
+
 }
