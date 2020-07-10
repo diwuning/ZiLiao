38b813a208f87145b0d08d9b1d37e73d8879ee0b
diwuning <153964989@qq.com>
2020-07-10 09:29:23 +0800

    Create AssetsUtils.java

---
+++ Utils/AssetsUtils.java
@@ -0,0 +1,14 @@
+package com.hongyang.firefapp.utils;
+
+import android.content.res.AssetManager;
+
+import com.google.gson.Gson;
+import com.hongyang.firefapp.base.MyApplication;
+import com.hongyang.firefapp.bean.InfoAlertBean;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AssetsUtils {
+}
